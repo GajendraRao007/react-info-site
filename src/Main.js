cbae6ce31cec5cd2eea8bb450841ae6291597ca7
Gajendra <raogajendra27@gmail.com>
2023-03-28 09:43:54 +0530

    Completed the REACT-INFO-SITE Project

--- src/Main.js
+++ src/Main.js
@@ -4,16 +4,17 @@ import React from "react"
 function Main(){
 
     return(
-        <div>
-        <h1>Fun facts about React</h1>
+        <main>
+
+        <h1 className="heading">Fun facts about React</h1>
         <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
+            <li className="txt">Was first released in 2013</li>
+            <li className="txt">Was originally created by Jordan Walke</li>
+            <li className="txt">Has well over 100K stars on GitHub</li>
+            <li className="txt">Is maintained by Facebook</li>
+            <li className="txt">Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
-        </div>
+        </main>
     )
 }
 
